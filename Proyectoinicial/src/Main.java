877ea40c2d98c9c3901fa782f7a88053bb411aac
vlasted <167018199+vlasted@users.noreply.github.com>
2025-10-12 17:50:53 +0200

    update

--- Proyectoinicial/src/Main.java
+++ Proyectoinicial/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola, este es mi primer programa en Java");
-
+        System.out.print("Esta es la segunda linea");
     }
 
 }
