04a98babba50fa52de40e673681bb5ebf3509a54
vlasted <167018199+vlasted@users.noreply.github.com>
2025-10-14 21:01:02 +0200

    update

--- Proyectoinicial/src/Main.java
+++ Proyectoinicial/src/Main.java
@@ -1,8 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        //TODO terminar de implementar la funcionalidad
+
+        String nombreCompleto= "Lenin Fonseca Ramirez";
+        int edad = 21;
+        edad = edad +1;
+        float altura = 1.71f;
+        double peso = 70.232894;
+
         System.out.println("Hola, este es mi primer programa en Java");
-        System.out.print("Esta es la segunda linea");
+        System.out.println("Mi nombre es "+nombreCompleto);
+        System.out.println("Mi edad es "+edad);
+        System.out.println("Mi altura es de "+altura);
+        System.out.println("Mi peso es de "+peso);
+
     }
 
 }
