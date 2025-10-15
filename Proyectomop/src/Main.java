8e6baaddb2d2e53d4a6d0434adc045388f283e2c
vlasted <167018199+vlasted@users.noreply.github.com>
2025-10-15 20:29:27 +0200

    update

--- Proyectomop/src/Main.java
+++ Proyectomop/src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Proyecto metodos");
+        System.out.println("Vamos a realizar llamadas a metodos");
+        saludar("Borja");
+        despedir();
+        saludar("Laura");
+        despedir();
 
+        Operaciones operacionesMatematicas = new Operaciones();
+        operacionesMatematicas.sumar(1,2);
+        operacionesMatematicas.sumar(4,4);
+    }
+
+    public static void saludar(String nombre){
+        System.out.println("Hola "+nombre);
+    }
+
+    public static void despedir(){
+        System.out.println("A cerrar el portatil");
     }
 }
---
+++ Proyectomop/src/Operaciones.java
@@ -0,0 +1,12 @@
+public class Operaciones {
+
+    public void sumar(int op1, int op2){
+        int resultado = op1+op2;
+        System.out.println("El resultado de la suma es "+resultado);
+    }
+
+    public void restar(int op1, int op2){
+        int resultado = op1-op2;
+        System.out.println("El resultado de la resta es "+resultado);
+    }
+}
