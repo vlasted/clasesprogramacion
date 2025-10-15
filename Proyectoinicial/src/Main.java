3964655caaa1de9fddd166c130d1a96e5fd9d7e6
vlasted <167018199+vlasted@users.noreply.github.com>
2025-10-15 19:21:18 +0200

    update

--- Proyectoinicial/src/Main.java
+++ Proyectoinicial/src/Main.java
@@ -9,12 +9,16 @@ public class Main {
         edad = edad +1;
         float altura = 1.71f;
         double peso = 70.232894;
+        char letraCarnetNumero = 'E';
+        boolean experiencia = true;
 
         System.out.println("Hola, este es mi primer programa en Java");
         System.out.println("Mi nombre es "+nombreCompleto);
         System.out.println("Mi edad es "+edad);
         System.out.println("Mi altura es de "+altura);
         System.out.println("Mi peso es de "+peso);
+        System.out.println("La letra de mi DNI es "+letraCarnetNumero);
+        System.out.println("Tengo experiencia "+experiencia);
 
     }
 
